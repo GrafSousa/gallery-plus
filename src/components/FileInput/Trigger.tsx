60c5ff94aaf8693fcaf666eb4af43b73e2478a82
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2026-01-13 12:01:28 +0100

    fix: add error validation when isn't a valid extension  to FileInputTrigger

--- src/components/FileInput/Trigger.tsx
+++ src/components/FileInput/Trigger.tsx
@@ -64,7 +64,7 @@ export function Trigger({ error }: TriggerProps) {
         </Typography>
       )}
 
-      {isValidSize && (
+      {!isValidSize && (
         <Typography className="text-accent-red">
           Arquivo maior que {maxFileSizeInMB}MB
         </Typography>
