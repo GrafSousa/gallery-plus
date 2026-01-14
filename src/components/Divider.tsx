c0e89c6769bc4a0d5119eea12b7e89e5551723da
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2026-01-14 16:41:20 +0100

    refactor: add orientation props to Divider

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,3 +1,22 @@
-export function Divider() {
-  return <div className="border-border-primary w-full border" />;
+import { tv, type VariantProps } from "tailwind-variants";
+
+const divider = tv({
+  base: "bg-border-primary",
+  variants: {
+    orientation: {
+      vertical: "h-full w-px",
+      horizontal: "w-full h-px",
+    },
+  },
+  defaultVariants: {
+    orientation: "horizontal",
+  },
+});
+
+type DividerVariants = VariantProps<typeof divider>;
+
+type DividerProps = React.ComponentProps<"div"> & DividerVariants;
+
+export function Divider({ orientation, className, ...props }: DividerProps) {
+  return <div className={divider({ orientation, className })} {...props} />;
 }
